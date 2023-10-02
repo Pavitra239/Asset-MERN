ed1bd90cdaf2a6c919cbf6ed3f60dfd608d2d764
Pavitra Modi <pavitramodi.it@charusat.ac.in>
2023-10-02 17:54:17 +0530

    confirm delete prompt

--- client/src/pages/DeleteProduct.jsx
+++ client/src/pages/DeleteProduct.jsx
@@ -3,11 +3,17 @@ import customFetch from "../utils/customFetch";
 import { redirect } from "react-router-dom";
 
 export const action = async ({ params }) => {
-  try {
-    await customFetch.delete(`/products/${params.id}`);
-    toast.success("Product deleted successfully");
-  } catch (error) {
-    toast.error(error?.response?.data?.msg || error.message);
+  const confirmDelete = window.confirm(
+    "Are you sure you want to delete the product?"
+  );
+  if (confirmDelete) {
+    try {
+      await customFetch.delete(`/products/${params.id}`);
+      toast.success("Product deleted successfully");
+    } catch (error) {
+      toast.error(error?.response?.data?.msg || error.message);
+    }
   }
+
   return redirect("/dashboard");
 };
